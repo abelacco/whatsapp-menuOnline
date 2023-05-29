18580b39f62ada2e25c28bde0c4208e2c24ec513
abelacco <abel3121@gmail.com>
2023-05-29 16:38:04 -0500

    gitognore

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = require('./app');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
+    console.log(`Server is running on PORT - ${PORT}`);
 });
 
